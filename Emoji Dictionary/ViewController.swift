5d7ebd9905f3f5f89cb6b10a875e0341c83279e5
Rebecca Fuller <bexter53@hotmail.com>
2018-01-15 15:37:39 +0100

    Initial commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Rebecca Fuller on 15/01/2018.
+//  Copyright © 2018 Rebecca Fuller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
